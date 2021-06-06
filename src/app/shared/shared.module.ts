9864e5195706978efec830a28a3483f781b469e2
otavioanovazzi <o186331@dac.unicamp.br>
2021-06-06 13:30:15 -0300

    1:20:14

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,6 +10,10 @@ import { HeaderComponent } from './layout/header/header.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports:
+  [
+    HeaderComponent,
   ]
 })
 export class SharedModule { }
